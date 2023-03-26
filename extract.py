dcd7f5245a8148e5edb1e90c330bebba738463ca
Rohit Prajapati <rohitrsp898@gmail.com>
2023-03-26 22:27:58 +0530

    Formatted The code

--- extract.py
+++ extract.py
@@ -1,20 +1,23 @@
-
 from pyspark.sql import SparkSession
 
 
 def extract(spark: SparkSession, type: str, source: str):
-    # Read data from mysql database 
-    if type=="JDBC":
-        output_df = spark.read.format("JDBC").options(url='jdbc:mysql://localhost/world',\
-            dbtable=source,driver='com.mysql.cj.jdbc.Driver',user='root',password='root').load()
+    """
+    Return Extracted Dataframe
+    :param spark: SparkSession
+    :param type: type of target
+    :param source: source path where data is going to extract
+    :return: DataFrame
+    """
+
+    if type == "JDBC":
+        # Read data from mysql database
+        output_df = spark.read.format("JDBC").options(url='jdbc:mysql://localhost/world', \
+                                                      dbtable=source, driver='com.mysql.cj.jdbc.Driver', user='root',
+                                                      password='root').load()
         return output_df
-    
-    if type=="CSV":
+
+    if type == "CSV":
         # read data from filesystem
-        output_df = spark.read.format("CSV").options(header=True,inferSchema=True).load(source)
+        output_df = spark.read.format("CSV").options(header=True, inferSchema=True).load(source)
         return output_df
-
-
-
-
-
--- transform.py
+++ transform.py
@@ -1,40 +1,38 @@
 from pyspark.sql import DataFrame
 
-# Rename all the columns
-'''
-:param df: input dataframe
-:param mapping_dict: dict of columns names
-:return: ouput dataframe
-'''
-def rename_cols(df: DataFrame, mapping_dict: dict) ->DataFrame:
-    
+
+def rename_cols(df: DataFrame, mapping_dict: dict) -> DataFrame:
+    """
+    # Rename all the columns
+    :param df: input dataframe
+    :param mapping_dict: Key value pair of column names (old:new)
+    :return: output Dataframe
+    """
     for key in mapping_dict.keys():
-        df=df.withColumnRenamed(key,mapping_dict.get(key))
-        
+        df = df.withColumnRenamed(key, mapping_dict.get(key))
+
     return df
 
 
-# get specific cols df
-'''
-:param df: input dataframe
-:param specific_cols: list of columns names
-:return: ouput dataframe
-'''
 def specific_cols(df: DataFrame, specific_cols: list):
+    """
+    Return only required columns
+    :param df: input Dataframe
+    :param specific_cols:
+    :return: Dataframe
+    """
     return df.select(specific_cols)
 
 
-# Join two dataframes
-'''
-:param left_df: input dataframe
-:param right_df: input dataframe
-:param ON_COLUMNS: list of columns to perform join
-:param JOIN_TYPE: Join type 
-:return: ouput dataframe
-'''
-def join_df(left_df: DataFrame, right_df: DataFrame, ON_COLUMNS:list, JOIN_TYPE: str)->DataFrame:
-    
-    output_df=left_df.alias("left_df").join(right_df.alias("right_df"), ON_COLUMNS, JOIN_TYPE)
-    
-    return output_df
+def join_df(left_df: DataFrame, right_df: DataFrame, ON_COLUMNS: list, JOIN_TYPE: str) -> DataFrame:
+    """
+    Return Join dataframe
+    :param left_df: left Dataframe
+    :param right_df: right Dataframe
+    :param ON_COLUMNS: Join Column
+    :param JOIN_TYPE: Join Type
+    :return: FInal Dataframe
+    """
+    output_df = left_df.alias("left_df").join(right_df.alias("right_df"), ON_COLUMNS, JOIN_TYPE)
 
+    return output_df
